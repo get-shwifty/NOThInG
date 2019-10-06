29616df1f9e26780384a6cd39ed815a65b29c380
Alexis Schad <schadoc_alex@hotmail.fr>
2019-10-06 16:27:22 +0200

    MReactive, new player dir textures

---
+++ NOThInG/src/types/Player/ondraw.js
@@ -0,0 +1,31 @@
+
+if(this.MMovable.dir === DIR.UP) {
+    if (this.MAntiGravity) {
+        this.tex = 'Player_Back_Gravity';
+    } else {
+        this.tex = 'Player_Back';
+    }
+    this.scale.x = 1;
+} else if(this.MMovable.dir === DIR.DOWN) {
+    if (this.MAntiGravity) {
+        this.tex = 'Player_Front_Gravity';
+    } else {
+        this.tex = 'Player_Front';
+    }
+    this.scale.x = 1;
+} else if(this.MMovable.dir === DIR.LEFT) {
+    self.lastMove = DIR.LEFT;
+    if (this.MAntiGravity) {
+        this.tex = 'Player_Side_Gravity';
+    } else {
+        this.tex = 'Player_Side';
+    }
+    this.scale.x = -1;
+} else if(this.MMovable.dir === DIR.RIGHT) {
+    if (this.MAntiGravity) {
+        this.tex = 'Player_Side_Gravity';
+    } else {
+        this.tex = 'Player_Side';
+    }
+    this.scale.x = 1;
+}
