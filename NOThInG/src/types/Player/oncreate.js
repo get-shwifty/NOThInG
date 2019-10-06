e819849aee40b7d413a163f9c4782bf85ba3a60f
Félix <someonefefe@gmail.com>
2019-10-07 00:20:57 +0200

    animation on moving

--- NOThInG/src/types/Player/oncreate.js
+++ NOThInG/src/types/Player/oncreate.js
@@ -1,5 +1,8 @@
 MMovable(this, false);
 MContainer(this, 4);
 this.animationSpeed = 0.08;
-
+this.moving = false;
+setInterval(() => {
+    this.moving = !this.moving;
+}, 3000)
 // this.MContainer.addElementByType('G');
--- NOThInG/src/types/Player/ondraw.js
+++ NOThInG/src/types/Player/ondraw.js
@@ -5,32 +5,41 @@ const playAnimation = function(obj, animName){
     }
 }
 
+this.animationSpeed = 0.08
+this.scale.x = this.MMovable.dir === DIR.LEFT ? -1: 1;
+
 if(this.MMovable.dir === DIR.UP) {
     if (this.MMovable.isAntiGravity()) {
         this.tex = 'Player_Gravity_Back';
+    }
+    else if(this.moving){
+        this.animationSpeed = 0.3
+        playAnimation(this, 'Player_Move_Back')
     } else {
         playAnimation(this, 'Player_Iddle_Back')
     }
-    this.scale.x = 1;
-} else if(this.MMovable.dir === DIR.DOWN) {
+} 
+else if(this.MMovable.dir === DIR.DOWN) {
     if (this.MMovable.isAntiGravity()) {
         this.tex = 'Play_Gravity_Front'
-    } else {
+    }
+    else if(this.moving){
+        this.animationSpeed = 0.3
+        playAnimation(this, 'Player_Move_Front')
+    } 
+    else {
         playAnimation(this, 'Player_Iddle_Front')
     }
-    this.scale.x = 1;
-} else if(this.MMovable.dir === DIR.LEFT) {
+} 
+else if(this.MMovable.dir === DIR.LEFT || this.MMovable.dir === DIR.RIGHT) {
     if (this.MMovable.isAntiGravity()) {
         this.tex = 'Player_Gravity_Side';
-    } else {
+    }
+    else if(this.moving){
+        this.animationSpeed = 0.3
+        playAnimation(this, 'Player_Move_Side')
+    } 
+    else {
         playAnimation(this, 'Player_Iddle_Side')
     }
-    this.scale.x = -1;
-} else if(this.MMovable.dir === DIR.RIGHT) {
-    if (this.MMovable.isAntiGravity()) {
-        this.tex = 'Player_Gravity_Side';
-    } else {
-        playAnimation(this, 'Player_Iddle_Side')
-    }
-    this.scale.x = 1;
-}
+} 
