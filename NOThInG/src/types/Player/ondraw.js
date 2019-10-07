c8d9ec0dae8206ab820dda02bb3dbdc225f68da2
Félix <someonefefe@gmail.com>
2019-10-07 11:39:41 +0200

    smoothing move animations

--- NOThInG/src/types/Player/ondraw.js
+++ NOThInG/src/types/Player/ondraw.js
@@ -1,46 +1,35 @@
-const playAnimation = function(obj, animName){
+const playAnimation = function(obj, animName, animSpeed){
     if (obj.tex !== animName){
+        obj.animationSpeed = animSpeed
         obj.tex = animName;
         obj.play();
     }
 }
-
-this.animationSpeed = 0.04
+const IDDLE_SPEED = 0.3
 this.scale.x = this.MMovable.dir === DIR.LEFT ? -1: 1;
 
 if(this.MMovable.dir === DIR.UP) {
     if (this.MMovable.isAntiGravity()) {
         this.tex = 'Player_Gravity_Back';
     }
-    else if(this.moving){
-        this.animationSpeed = 0.3
-        playAnimation(this, 'Player_Move_Back')
-    } else {
-        playAnimation(this, 'Player_Iddle_Back')
+    else {
+        playAnimation(this, 'Player_Move_Back', IDDLE_SPEED)
     }
 } 
 else if(this.MMovable.dir === DIR.DOWN) {
     if (this.MMovable.isAntiGravity()) {
         this.tex = 'Player_Gravity_Front';
     }
-    else if(this.moving){
-        this.animationSpeed = 0.3
-        playAnimation(this, 'Player_Move_Front')
-    } 
     else {
-        playAnimation(this, 'Player_Iddle_Front')
+        playAnimation(this, 'Player_Move_Front', IDDLE_SPEED)
     }
 }
 else if(this.MMovable.dir === DIR.LEFT || this.MMovable.dir === DIR.RIGHT) {
     if (this.MMovable.isAntiGravity()) {
         this.tex = 'Player_Gravity_Side';
     }
-    else if(this.moving){
-        this.animationSpeed = 0.3
-        playAnimation(this, 'Player_Move_Side')
-    } 
     else {
-        playAnimation(this, 'Player_Iddle_Side')
+        playAnimation(this, 'Player_Move_Side', IDDLE_SPEED)
     }
 } 
 
