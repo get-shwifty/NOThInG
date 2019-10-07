d9c3a7cdb9e6cb94927fe253d1bc29832da8366e
Damiend <dfdelmas@gmail.com>
2019-10-07 15:04:11 +0200

    adding sounds

--- NOThInG/src/types/Box/oncreate.js
+++ NOThInG/src/types/Box/oncreate.js
@@ -1,2 +1,15 @@
 MMovable(this);
 MContainer(this, 3);
+
+this.MEvent.on('moveStart', () => {
+    console.log("moveStart");
+    if(! this.MMovable.isAntiGravity()){
+       ct.sound.spawn("crate") 
+    }
+});
+this.MEvent.on('moveEnd', () => {
+    console.log("moveEnd");
+    if(this.MMovable.isAntiGravity()){
+       ct.sound.spawn("impact") 
+    }
+});
