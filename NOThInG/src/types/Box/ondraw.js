49fbd320c88431ae023aa2c16b0eb74c48211764
Alexis Schad <schadoc_alex@hotmail.fr>
2019-10-07 14:40:10 +0200

    save

--- NOThInG/src/types/Box/ondraw.js
+++ NOThInG/src/types/Box/ondraw.js
@@ -1,10 +1,1 @@
 this.MContainer.onDraw();
-
-this.shadow.visible = false
-this.body.y = 0
-if (this.MMovable.isAntiGravity()) {
-    this.body.y = -30 + Math.sin(this.count / 8) * 2.5;
-    this.shadow.visible = true
-    this.shadow.scale.x -= Math.sin(this.count / 8) * 0.02
-    this.count++
-}
