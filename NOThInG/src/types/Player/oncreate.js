b589b670ecc99f6a42a38cc7bd191e51d8d452b5
feldvale <feldvale@utc.fr>
2019-10-07 17:37:14 +0200

    radioactivity working; need UI too

--- NOThInG/src/types/Player/oncreate.js
+++ NOThInG/src/types/Player/oncreate.js
@@ -3,23 +3,6 @@ MContainer(this, 4);
 
 this.body.tex = -1;
 
-this.MEvent.on('moveStart', () => {
-    if(this.remainingOxygen === 0) {
-        console.log("DYYYYYYYYING, breathing is not an option !!!");
-    }
-});
-this.MEvent.on('moveEnd', () => {
-    if(!this.breathing) {
-        this.remainingOxygen--;
-    }
-    // console.log("Oxygen: ", this.remainingOxygen);
-    if(this.MMovable.isAntiGravity()){
-        if(utils.getNextDir(this, this.MMovable.dir, 'MObstacle')) {
-            ct.sound.spawn("impact") ;
-        }
-    }
-});
-
 // *** Oxygen ***
 this.MAX_OXYGEN = 4; 
 this.remainingOxygen = this.MAX_OXYGEN;
@@ -40,3 +23,45 @@ this.MEvent.on("elementDropped", (el, remainingEls) => {
     }
    this.breathing = false;
 });
+
+// *** Radioactivity ***
+this.MAX_RADIOACTIVITY = 5; 
+this.currentRaioactivity = 0;
+this.numRadEl = 0;
+
+this.MEvent.on("elementTaken", (el) => {
+    if (el.MElement.getType().type == EL.Th.type) {
+        this.numRadEl++;
+    }
+});
+
+this.MEvent.on("elementDropped", (el, remainingEls) => {
+    if (el.MElement.getType().type == EL.Th.type) {
+        this.numRadEl--;
+    }
+});
+
+// *** MEvent : moving ***
+this.MEvent.on('moveStart', () => {
+    if(this.remainingOxygen === 0) {
+        console.log("DYYYYYYYYING, breathing is not an option !!!");
+    }
+    if(this.currentRaioactivity === this.MAX_RADIOACTIVITY) {
+        console.log("DYYYYYYYYING, Tchernobyl got you !!!");
+    }
+});
+this.MEvent.on('moveEnd', () => {
+    if(!this.breathing) {
+        this.remainingOxygen--;
+    }
+    this.currentRaioactivity += this.numRadEl;
+    console.log("Radioactivity: ", this.currentRaioactivity);
+    // console.log("Oxygen: ", this.remainingOxygen);
+    if(this.MMovable.isAntiGravity()){
+        if(utils.getNextDir(this, this.MMovable.dir, 'MObstacle')) {
+            ct.sound.spawn("impact") ;
+        }
+    }
+});
+
+
