6553c129bea2c40bc46efaa3be3fe34586be2c2a
feldvale <feldvale@utc.fr>
2019-10-08 00:20:03 +0200

    removing Valentin's room

--- NOThInG/src/rooms/Valentin/oncreate.js
+++
@@ -1,6 +0,0 @@
-let player = ct.types.list.Player[0];
-player.MContainer.addElementByCst(EL.Th);
-let box = ct.types.list.Box[0];
-box.MContainer.addElementByCst(EL.O);
-box.MContainer.addElementByCst(EL.Th);
-
