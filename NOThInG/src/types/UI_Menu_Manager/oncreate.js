bdb45f0e5157e171e2b754a77387130982541767
feldvale <oss117.hb@gmail.com>
2019-10-14 17:15:20 +0200

    WIP Menu Manager

--- NOThInG/src/types/UI_Menu_Manager/oncreate.js
+++ NOThInG/src/types/UI_Menu_Manager/oncreate.js
@@ -4,3 +4,52 @@ const POS = {
     secondB: { x: 1071, y: 975},
     thirdB: { x: 1071, y: 1195},
 };
+
+this.uiElements = [];
+
+this.showStartingMenu = function () {
+    if (uiElements.length > 0) {
+        this.clearMenu();
+    }
+    this.uiElements.push(ct.types.make("UI_Menu", POS.title.x, POS.title.y));
+    this.uiElements.push(ct.types.make("UI_Start", POS.firstB.x, POS.firstB.y));
+    this.uiElements.push(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y));
+    this.uiElements.push(ct.types.make("UI_Exit", POS.thirdB.x, POS.thirdB.y));
+};
+
+this.showGameMenu = function () {
+    if (uiElements.length > 0) {
+        this.clearMenu();
+    }
+    this.uiElements.push(ct.types.make("UI_Menu", POS.title.x, POS.title.y));
+    this.uiElements.push(ct.types.make("UI_Restart", POS.firstB.x, POS.firstB.y));
+    this.uiElements.push(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y));
+    this.uiElements.push(ct.types.make("UI_Menu", POS.thirdB.x, POS.thirdB.y));
+};
+
+this.showWinMenu = function () {
+    if (uiElements.length > 0) {
+        this.clearMenu();
+    }
+    this.uiElements.push(ct.types.make("UI_Win", POS.title.x, POS.title.y));
+    this.uiElements.push(ct.types.make("UI_Next", POS.firstB.x, POS.firstB.y));
+    this.uiElements.push(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y));
+    this.uiElements.push(ct.types.make("UI_Menu", POS.thirdB.x, POS.thirdB.y));
+};
+
+this.showLoseMenu = function () {
+    if (uiElements.length > 0) {
+        this.clearMenu();
+    }
+    this.uiElements.push(ct.types.make("UI_Lose", POS.title.x, POS.title.y));
+    this.uiElements.push(ct.types.make("UI_Restart", POS.firstB.x, POS.firstB.y));
+    this.uiElements.push(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y));
+    this.uiElements.push(ct.types.make("UI_Menu", POS.thirdB.x, POS.thirdB.y));
+};
+
+this.clearMenu = function () {
+    this.uiElements.foreach(function (el) {
+        el.kill = true;
+    });
+    uiElements = [];
+};
