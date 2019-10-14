d73e31d27c8d0a973be10108b7fc90298b319a3c
feldvale <12518162+feldvale@users.noreply.github.com>
2019-10-14 20:16:41 +0200

    UI scaling

--- NOThInG/src/types/UI_Menu_Manager/oncreate.js
+++ NOThInG/src/types/UI_Menu_Manager/oncreate.js
@@ -1,8 +1,16 @@
+const SCALE = Math.min(ct.viewWidth / 2142, ct.viewHeight / 1428);
+
+this.scaling = function(el) {
+    el.scale.x *= SCALE;
+    el.scale.y *= SCALE;
+    return el;
+};
+
 const POS = {
-    title: { x: 1047, y: 350},
-    firstB: { x: 1071, y: 750},
-    secondB: { x: 1071, y: 975},
-    thirdB: { x: 1071, y: 1195},
+    title: { x: ct.viewWidth / 2, y: 350 * SCALE},
+    firstB: { x: ct.viewWidth / 2, y: 750 * SCALE},
+    secondB: { x: ct.viewWidth / 2, y: 975 * SCALE},
+    thirdB: { x: ct.viewWidth / 2, y: 1195 * SCALE},
 };
 
 this.uiElements = [];
@@ -18,10 +26,10 @@ this.showStartingMenu = function () {
     if (this.uiElements.length > 0) {
         this.clearMenu();
     }
-    this.uiElements.push(ct.types.make("UI_Menu", POS.title.x, POS.title.y));
-    this.uiElements.push(ct.types.make("UI_Start", POS.firstB.x, POS.firstB.y));
-    this.uiElements.push(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y));
-    this.uiElements.push(ct.types.make("UI_Exit", POS.thirdB.x, POS.thirdB.y));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Menu", POS.title.x, POS.title.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Start", POS.firstB.x, POS.firstB.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Exit", POS.thirdB.x, POS.thirdB.y)));
     // PIXI.ticker.shared.speed = 0;
     // ct.pixiApp.ticker.speed = 0;
 };
@@ -57,10 +65,10 @@ this.showGameMenu = function () {
     }
     this.showModalBackground();
     this.uiElements[0].tint = 0x373737;
-    this.uiElements.push(ct.types.make("UI_Menu", POS.title.x, POS.title.y));
-    this.uiElements.push(ct.types.make("UI_Restart", POS.firstB.x, POS.firstB.y));
-    this.uiElements.push(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y));
-    this.uiElements.push(ct.types.make("UI_Menu_Button", POS.thirdB.x, POS.thirdB.y));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Menu", POS.title.x, POS.title.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Restart", POS.firstB.x, POS.firstB.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Menu_Button", POS.thirdB.x, POS.thirdB.y)));
     // PIXI.ticker.shared.speed = 0;
     // ct.pixiApp.ticker.speed = 0;
 };
@@ -71,10 +79,10 @@ this.showWinMenu = function () {
     }
     this.showModalBackground();
     this.uiElements[0].tint = 0x224f22;
-    this.uiElements.push(ct.types.make("UI_Win", POS.title.x, POS.title.y));
-    this.uiElements.push(ct.types.make("UI_Next", POS.firstB.x, POS.firstB.y));
-    this.uiElements.push(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y));
-    this.uiElements.push(ct.types.make("UI_Menu_Button", POS.thirdB.x, POS.thirdB.y));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Win", POS.title.x, POS.title.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Next", POS.firstB.x, POS.firstB.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Menu_Button", POS.thirdB.x, POS.thirdB.y)));
     // PIXI.ticker.shared.speed = 0;
     // ct.pixiApp.ticker.speed = 0;
 };
@@ -85,10 +93,10 @@ this.showLoseMenu = function () {
     }
     this.showModalBackground();
     this.uiElements[0].tint = 0x511313;
-    this.uiElements.push(ct.types.make("UI_Lose", POS.title.x, POS.title.y));
-    this.uiElements.push(ct.types.make("UI_Restart", POS.firstB.x, POS.firstB.y));
-    this.uiElements.push(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y));
-    this.uiElements.push(ct.types.make("UI_Menu_Button", POS.thirdB.x, POS.thirdB.y));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Lose", POS.title.x, POS.title.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Restart", POS.firstB.x, POS.firstB.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Select", POS.secondB.x, POS.secondB.y)));
+    this.uiElements.push(this.scaling(ct.types.make("UI_Menu_Button", POS.thirdB.x, POS.thirdB.y)));
     // PIXI.ticker.shared.speed = 0;
     // ct.pixiApp.ticker.speed = 0;
 };
