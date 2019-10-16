8fb5abaa96f1dfdffc5a80ce49903dc820ac54dc
Félix <someonefefe@gmail.com>
2019-10-16 09:12:38 +0200

    base new MTile

---
+++ NOThInG/src/scripts/MTile/code.js
@@ -0,0 +1,8 @@
+const MTile = (self, baseSprite) => {
+    const isSameType = [[0,0,0], [0,0], [0,0,0]]
+    const arround = utils.getUnderDistance(self, 1)
+    const own = self.type
+    arround.forEach(obj => {
+        // do stuff
+    })
+}
